28c7c4c684d739df9010698aff69451700d77f39
Yoshi <dleonavichus@mail.ru>
2022-10-10 22:32:55 +0300

    test

--- lib/app/modules/shedule/view.dart
+++ lib/app/modules/shedule/view.dart
@@ -1,10 +1,8 @@
-import 'package:dark_todo/app/core/values/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:table_calendar/table_calendar.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../core/utils/extensions.dart';
 import '../detail/view.dart';
 import '../home/controller.dart';
@@ -19,61 +17,22 @@ class ShedulePage extends StatefulWidget {
 class _ShedulePageState extends State<ShedulePage> {
   final homeCtrl = Get.find<HomeController>();
 
-  DateTime selectedDay = DateTime.now();
-  DateTime focusedDay = DateTime.now();
-  CalendarFormat calendarFormat = CalendarFormat.week;
-
   @override
   Widget build(BuildContext context) {
-    final tag = Localizations.maybeLocaleOf(context)?.toLanguageTag();
     final theme = Theme.of(context);
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.all(10.w),
+              padding: EdgeInsets.only(top: 15.w, bottom: 10.w),
               child: Text(
-                'Невыполненные задачи',
+                AppLocalizations.of(context)!.timeTask,
                 style: theme.textTheme.headline2,
                 overflow: TextOverflow.ellipsis,
                 textAlign: TextAlign.center,
               ),
             ),
-            // TableCalendar(
-            //   startingDayOfWeek: StartingDayOfWeek.monday,
-            //   firstDay: DateTime(2022, 09, 01),
-            //   lastDay: DateTime(2100, 09, 01),
-            //   focusedDay: selectedDay,
-            //   locale: '$tag',
-            //   selectedDayPredicate: (day) {
-            //     return isSameDay(selectedDay, day);
-            //   },
-            //   onDaySelected: (selected, focused) {
-            //     setState(
-            //       () {
-            //         selectedDay = selected;
-            //         focusedDay = focused;
-            //       },
-            //     );
-            //   },
-            //   onPageChanged: (focused) {
-            //     focusedDay = focused;
-            //   },
-            //   availableCalendarFormats: {
-            //     CalendarFormat.month: AppLocalizations.of(context)!.month,
-            //     CalendarFormat.twoWeeks: AppLocalizations.of(context)!.two_week,
-            //     CalendarFormat.week: AppLocalizations.of(context)!.week
-            //   },
-            //   calendarFormat: calendarFormat,
-            //   onFormatChanged: (format) {
-            //     setState(
-            //       () {
-            //         calendarFormat = format;
-            //       },
-            //     );
-            //   },
-            // ),
             Divider(
               color: theme.dividerColor,
               height: 10.w,
