a54237c0ca9acac8a6b8a54ab97435cf8ee928a2
Yoshi <dleonavichus@mail.ru>
2023-05-07 15:53:59 +0300

    icon

--- lib/app/services/notification.dart
+++ lib/app/services/notification.dart
@@ -5,6 +5,10 @@ import 'package:timezone/timezone.dart' as tz;
 class NotificationShow {
   Future showNotification(
       int id, String title, String body, DateTime? date) async {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestPermission();
     AndroidNotificationDetails androidNotificationDetails =
         const AndroidNotificationDetails(
       'ToDark',
@@ -24,7 +28,7 @@ class NotificationShow {
       notificationDetails,
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime,
-      androidAllowWhileIdle: true,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       payload: 'notlification-payload',
     );
   }
