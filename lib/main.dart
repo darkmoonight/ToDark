ee3a03ddae3d5f2856d43dc3fb76f6a1cebcd182
Yoshi <dleonavichus@mail.ru>
2024-03-10 00:18:34 +0300

    Add locale tr_TR

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,16 @@ Locale locale = const Locale('en', 'US');
 
 final List appLanguages = [
   {'name': 'العربية', 'locale': const Locale('ar', 'AR')},
+  {'name': 'Deutsch', 'locale': const Locale('de', 'DE')},
   {'name': 'English', 'locale': const Locale('en', 'US')},
   {'name': 'Español', 'locale': const Locale('es', 'ES')},
   {'name': 'Français', 'locale': const Locale('fr', 'FR')},
   {'name': 'Italiano', 'locale': const Locale('it', 'IT')},
   {'name': 'فارسی', 'locale': const Locale('fa', 'IR')},
   {'name': 'Русский', 'locale': const Locale('ru', 'RU')},
+  {'name': 'Türkçe', 'locale': const Locale('tr', 'TR')},
   {'name': '中文', 'locale': const Locale('zh', 'CN')},
   {'name': '中国传统台湾', 'locale': const Locale('zh', 'TW')},
-  {'name': 'Deutsch', 'locale': const Locale('de', 'DE')},
 ];
 
 void main() async {
