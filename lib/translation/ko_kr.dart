15248553f9712b6fa1a84f57ccc71de12d4d4bd5
tsyqax6 <122242962+tsyqax@users.noreply.github.com>
2024-05-31 00:40:57 +0900

    Update ko_kr.dart

--- lib/translation/ko_kr.dart
+++ lib/translation/ko_kr.dart
@@ -1,1 +1,36 @@
-
+class EnUs {
+  Map<String, String> get messages => {
+        'title1': '할 일 정리',
+        'title2': '유저 친화적 디자인',
+        'title3': '문의하기',
+        'subtitle1':
+            '우리 앱에서는, 할 일들을 분류하고, 하나씩 완료해갈 수 있습니다.',
+        'subtitle2':
+            '모든 탐색은 가능한 한 편리하고 빠르게 앱과 상호 작용할 수 있도록 만들어졌습니다.',
+        'subtitle3':
+            '만약 어떠한 문제라도 거슬리신다면, 이메일 또는 앱 리뷰에서 문의해주세요.',
+        'getStart': '시작하기',
+        'next': '다음',
+        'week': '주간',
+        'two_week': '2주간',
+        'month': '월간',
+        'updateTodo': '할 일 변경',
+        'addTodo': '할 일 추가',
+        'completedTodo': '할 일 완료',
+        'deletedTodo': '할 일 삭제',
+        'deletedTodoQuery': '정말로 이 할 일을 삭제하시겠나요?',
+        'delete': '삭제',
+        'cancel': '취소',
+        'editing': '편집',
+        'name': '이름',
+        'validateName': '이름을 입력해주세요',
+        'description': '설명',
+        'timeComplete': '만료일',
+        'addCategory': '카테고리 추가',
+        'deleteCategory': '카테고리 삭제',
+        'deleteCategoryQuery': '정말로 이 카테고리를 삭제하시겠나요?',
+        'todo': '할 일',
+        'allTodos': '모든 할 일',
+        'create': '만들기',
+        'completed': '완료됨',
+        'categories': '카테고리',
